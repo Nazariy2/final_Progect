9aee700f3edcde1d8d18774d4222a386c7d576e6
Nazariy2 <nazrud23@gmail.com>
2025-05-15 23:33:25 +0300

    a

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -6144,7 +6144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51333" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62789" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
